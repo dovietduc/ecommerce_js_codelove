36150a01e629a1851c582f5ec3e9fdf8571897d6
ducdo <tvietduc122@gmail.com>
2023-09-26 15:44:03 +0700

    tach logic

--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -22,17 +22,40 @@ function handleSignUpClick(event) {
         // validate not empty
         if(name === 'name') {
             // require
+            if(!require(inputSelector)) {
+                showError(inputSelector, 'Tên không được để trống');
+            } else {
+                // show success
+                showSuccess(inputSelector);
+            }
         } else if(name === 'email') {
-            // 1. require
-            // 2. minlength
-            // 3. regex vlaidate email
+            if(!require(inputSelector)) {
+                showError(inputSelector, 'email không được để trống');
+            } else if(!minlength(inputSelector)) {
+                showError(inputSelector, `email tối thiểu ${inputSelector.getAttribute('min_length')} kí tự`);
+            } else if(!emailRegex(inputSelector)) {
+                showError(inputSelector, 'email khồng đúng định dạng');
+            } else {
+                showSuccess(inputSelector);
+            }
         } else if(name === 'password') {
-            // 1. require
-            // 2. minlength
+            if(!require(inputSelector)) {
+                showError(inputSelector, 'password không được để trống');
+            } else if(!minlength(inputSelector)) {
+                showError(inputSelector, `password tối thiểu ${inputSelector.getAttribute('min_length')} kí tự`);
+            } else {
+                showSuccess(inputSelector);
+            }
         } else {
-            // 1. require
-            // 2. minlength
-            // 2. compare pass
+            if(!require(inputSelector)) {
+                showError(inputSelector, 'confirm password không được để trống');
+            } else if(!minlength(inputSelector)) {
+                showError(inputSelector, `confirm password tối thiểu ${inputSelector.getAttribute('min_length')} kí tự`);
+            } else if(!comparePass(inputSelector)) {
+                showError(inputSelector, 'confirm password không trùng với password');
+            } else {
+                showSuccess(inputSelector);
+            }
         }
       
     }
@@ -40,109 +63,52 @@ function handleSignUpClick(event) {
     // kiểm tra không có ô input nào có lỗi validate
     // 1. lưu user vào localStorage
     // 2. redirect đến màn hình login
-    for(let i = 0; i < errorMessageAll.length; i++) {
-        if(errorMessageAll[i].textContent !== '') {
-            isFormValid = false;
-            break;
-        }
-    }
-    if(isFormValid) {
-        console.log('to page login');
-    }
-    
 
 
 }
 
+// rule require
+// output: return true or false
+function require(inputSelector) {
+    return inputSelector.value ? true : false;
+}
+function minlength(inputSelector) {
+    let minLength = inputSelector.getAttribute('min_length');
+    let inputValue = inputSelector.value;
+    if(inputValue.length < minLength) {
+        return false;
+    }
+    return true;
+}
+function emailRegex(inputSelector) {
+    let inputValue = inputSelector.value;
+    return regexEmail.test(inputValue);
+}
+
+function comparePass(inputSelector) {
+    let valueConfirmPass = inputSelector.value;
+    let passwordSelector = document.querySelector('.' + inputSelector.getAttribute('selector_compare'));
+    let valuePassword = passwordSelector.value;
+
+    return valueConfirmPass === valuePassword;
+
+}
 
 
-function showSuccess(inputSelector, divMessageSelector) {
+function showError(inputSelector, message = null) {
+    // 1. Hiển thị mầu đỏ cho ô input
+    inputSelector.classList.add('error');
+    // 2. Thêm nội dung lỗi cho div mesage dưới ô input
+    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
+    divMessageSelector.textContent = message;
+}
+
+function showSuccess(inputSelector) {
+    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
     inputSelector.classList.remove('error');
     divMessageSelector.textContent = '';
 }
 
-// rule compare data
-function compareFieldsValidate(inputSelector, name, message) {
-    let isValid = true;
-    let valueInput = inputSelector.value;
-    let compareSelectorClass = inputSelector.getAttribute('selector_compare');
-    let compareSelector = document.querySelector('.' + compareSelectorClass);
-    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
-    if(compareSelector.value !== valueInput) {
-        isValid = false;
-        inputSelector.classList.add('error');
-        // hiển thị message lỗi
-        let messageError = 'dữ liệu nhập ở ' + name + ' không trùng với dữ liệu nhập ở ' + compareSelectorClass;
-        if(message) {
-            messageError = message;
-        }
-        divMessageSelector.textContent = messageError;
-    }
-    return isValid;
-}
-
-// rule require validate
-function requireValidate(inputSelector, name, message) {
-
-    // 1. Kiểm tra xem rule có hợp lệ hay không?
-    // 2. Nếu không hợp lệ, hiển thị thông báo lỗi dưới ô input
-
-    let isValid = true;
-    let valueInput = inputSelector.value;
-    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
-    if(valueInput === '') {
-        isValid = false;
-        // thêm viền đỏ cho input
-        inputSelector.classList.add('error');
-        // hiển thị message lỗi
-        let messageError = name + ' không được để trống';
-        if(message) {
-            messageError = message;
-        }
-        divMessageSelector.textContent = messageError;
-    }
-    return isValid;
-}
-
-// rule validate email
-function emailRegexValidate(inputSelector, name, message) {
-    let isValid = true;
-    let valueInput = inputSelector.value;
-    let isValidRegex = regexEmail.test(valueInput);
-    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
-    if(isValidRegex === false) {
-        isValid = false;
-        inputSelector.classList.add('error');
-        let messageError = name + ' không phải định dạng email hợp lệ';
-        if(message) {
-            messageError = message;
-        }
-        
-        divMessageSelector.textContent = messageError;
-    }
-    return isValid;
-}
-
-// rule validate min-length
-function minLengthValidate(inputSelector, name, message) {
-    let isValid = true;
-    let valueInput = inputSelector.value;
-    let divMessageSelector = inputSelector.closest('.form-group').querySelector('.error_message');
-    // optional
-    let minLength = inputSelector.getAttribute('min_length');
-
-    if(valueInput.length < minLength) {
-        isValid = false;
-        inputSelector.classList.add('error');
-        let messageError = name + ' tối thiểu ' + minLength + ' kí tự';
-        if(message) {
-            messageError = message;
-        }
-
-        divMessageSelector.textContent = messageError;
-    }
-    return isValid;
-}
 
 
 
