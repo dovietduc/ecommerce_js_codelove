c2026b724e587d769cc745fc5eef61e4490beb48
ducdo <tvietduc122@gmail.com>
2023-10-02 21:01:25 +0700

    add data

--- assets/js/pages/core/validate.js
+++ assets/js/pages/core/validate.js
@@ -1,3 +1,27 @@
 function Validate(options) {
-    console.log('options', options);
+
+    // 1. Lấy ra container bao đóng form
+    const container = document.querySelector(options.container);
+    // 2. Tất cả các elements khác query dựa vào container
+    const btnSignUpSelector = container.querySelector('.btn-signup');
+    const rules = options.rules;
+    const messages = options.messages;
+    console.log(rules, messages);
+
+
+
+    function initEventAndData() {
+        btnSignUpSelector.addEventListener('click', handleSignUpClick);
+    }
+
+    function handleSignUpClick(event) {
+        event.preventDefault();
+    }
+
+
+
+    
+
+    // add event listener + data init
+    initEventAndData();
 }
--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -1,5 +1,28 @@
 let signupInstanceValidate = new Validate(
     {
-        container: "#page_regsiter"
+        container: ".form_register",
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                minlength: 3,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 8 
+            },
+            confirm_password: {
+                required: true,
+                minlength: 8,
+                equal_to: 'password'
+            }
+        },
+        messages: {
+            name_required: 'Tên không được để trống',
+            email_required: 'Email không được để trống'
+        }
     }
 );
