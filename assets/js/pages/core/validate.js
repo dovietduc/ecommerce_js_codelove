1bc227d999abea0a06a66df5c03a1a7a0d8435fb
ducdo <tvietduc122@gmail.com>
2023-10-04 14:47:47 +0700

    formayt code

--- assets/js/pages/core/validate.js
+++ assets/js/pages/core/validate.js
@@ -1,12 +1,18 @@
 function Validate(options) {
-    // 1. Lấy ra container bao đóng form
-    const container = document.querySelector(options.container);
-    // 2. Tất cả các elements khác query dựa vào container
-    const btnSignUpSelector = container.querySelector('.btn-signup');
+    // khai báo biến có options
+    const btnClassSubmit = options.btnClassSubmit || 'btn-signup';
+    const errorClass = options.errorClass || 'error';
+    const errorMessageClass = options.errorMessageClass || 'error_message';
+    const formGroupClass = options.formGroupClass || 'form-group';
     const rules = options.rules;
     const messages = options.messages;
-    let errors;
 
+    // truy vấn DOM của thư viện
+    const container = document.querySelector(options.container);
+    const btnSignUpSelector = container.querySelector('.' + btnClassSubmit);
+
+   
+    let errors;
     const rulesMethod = {
         required: function(valueInput, valueRule) {
             return valueInput !== '';
@@ -61,17 +67,17 @@ function Validate(options) {
     }
 
     function resetErrors(inputSelector) {
-        inputSelector.classList.remove('error');
-        let divError = inputSelector.closest('.form-group').querySelector('.error_message');
+        inputSelector.classList.remove(errorClass);
+        let divError = inputSelector.closest(`.${formGroupClass}`).querySelector(`.${errorMessageClass}`);
         divError.textContent = '';
     }
 
     function showErrors() {
         errors.forEach(function(element) {
             let inputElement = element.elementError;
-            let divError = inputElement.closest('.form-group').querySelector('.error_message');
+            let divError = inputElement.closest(`.${formGroupClass}`).querySelector(`.${errorMessageClass}`);
 
-            inputElement.classList.add('error');
+            inputElement.classList.add(errorClass);
             divError.textContent = element.message;
         });
     }
