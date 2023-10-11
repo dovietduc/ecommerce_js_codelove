b2e497226a25a969d8fc93f91fff88a56fe4bf3a
ducdo <tvietduc122@gmail.com>
2023-10-11 10:00:56 +0700

    validate login page

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,5 +1,36 @@
+function rules() {
+    return {
+        email: {
+            required: true,
+            minlength: 3,
+            regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        },
+        password: {
+            required: true,
+            minlength: 8
+        }
+    };
+}
+
+
+function messages() {
+    return {
+        name_required: 'Tên không được để trống',
+        email_required: 'Email không được để trống',
+        email_regex: 'Email không đúng định dạng'
+    };
+}
+
+function validateSuccess() {
+    console.log('validate success');
+}
+
 let loginInstanceValidate = new Validate(
     {
-        container: "#page_login"
+        container: ".login_form",
+        btnClassSubmit: 'btn-login',
+        rules: rules(),
+        messages: messages(),
+        success: validateSuccess
     }
 );
