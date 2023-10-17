83306be53fed74a4cb1ff42b00fda27adadc9939
duc do <tvietduc122@gmail.com>
2023-10-17 16:58:02 +0700

    show cate

--- assets/js/pages/admin-category.js
+++ assets/js/pages/admin-category.js
@@ -1,3 +1,23 @@
+function showDataCateFromLocal() {
+    // 1. Lấy toàn bộ danh mục trong local
+    const categories = JSON.parse(localStorage.getItem('categories'));
+    // 2. Xây dựng cấu trúc html cho danh mục
+    let htmlResult = '';
+    categories.forEach(function(element) {
+        htmlResult = htmlResult + `<tr>
+                        <td>${element.name}</td>
+                        <td>
+                            <button class="btn_common btn_edit">Edit</button>
+                            <button class="btn_common btn_delete">Delete</button>
+                        </td>
+                    </tr>`;
+
+    });
+
+    // 2. Đưa kết quả toàn bộ danh mục vào tbody của table
+    document.querySelector('.category_table').innerHTML = htmlResult;
+}
+
 function validateSuccess() {
     // 1. Lấy ra thông tin của danh mục
     const nameCategory = document.querySelector('.category_name').value;
@@ -11,9 +31,13 @@ function validateSuccess() {
     const categoriesUpdate = [...categories, newCate];
     // 4. Lưu vào trong local
     localStorage.setItem('categories', JSON.stringify(categoriesUpdate));
+    // 5. Hiển thị dữ liệu ngay lập tức khi thêm thành công
+    showDataCateFromLocal();
 };
 
 
+// Hiển thị dữ liệu category từ local
+showDataCateFromLocal();
 let validateCategory = new Validate(
     {
         container: "#category_form_add",
